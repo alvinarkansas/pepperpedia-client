554c1824032b399c12029c7590606f12618dd1a1
alvinarkansas <alvinarkansas@gmail.com>
2020-05-19 23:25:48 +0700

    added load more button on home ^^

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FETCH_RECIPES } from '../store/action';
 import Jumbotron from '../components/Jumbotron';
 import RecipeCard from '../components/RecipeCard';
 import Notification from '../components/Notification';
+import Button from '../components/Button';
 import BeatLoader from 'react-spinners/BeatLoader';
 
 export default function Home() {
@@ -14,9 +15,16 @@ export default function Home() {
   const notifMessage = useSelector(state => state.notifMessage);
   const recipesLoading = useSelector(state => state.recipesLoading);
 
+  const [visible, setVisible] = useState(9);
+
   useEffect(() => {
     dispatch(FETCH_RECIPES())
   }, [dispatch])
+
+  const loadMore = () => {
+    let more = visible + 6;
+    setVisible(more);
+  }
 
   return (
     <div>
@@ -31,9 +39,10 @@ export default function Home() {
             loading={recipesLoading}
           />
         </div>
-        <div className="grid-container">
-          {recipes.map(recipe => <RecipeCard key={recipe.id} recipe={recipe} />)}
+        <div className="grid-container mb-2">
+          {recipes.slice(0, visible).map(recipe => <RecipeCard key={recipe.id} recipe={recipe} />)}
         </div>
+        {recipes.length !== visible && <Button caption="Load More" onClick={loadMore} />}
       </main>
       <Notification message={notifMessage} open={notifOpen} />
     </div>
